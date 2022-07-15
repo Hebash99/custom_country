2c7a19bb3a001ea12ef539d3baf02bf4413a32b1
Hebash99 <95986694+Hebash99@users.noreply.github.com>
2022-07-16 00:53:34 +0300

    Update country_state_city_picker.dart

--- lib/src/country_state_city_picker.dart
+++ lib/src/country_state_city_picker.dart
@@ -113,7 +113,7 @@ class _CountryStateCityPickerState extends State<CountryStateCityPicker> {
               borderRadius: BorderRadius.circular(20.0),
             ),
             isDense: true,
-            hintText: (initialCountry==null)?'Country':initialCountry as String,
+            hintText: 'Country',
          hintStyle: const TextStyle(
            fontSize: 20,
            fontWeight: FontWeight.w700,
