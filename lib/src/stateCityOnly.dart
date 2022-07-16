4c78b9e4e99be3792b57b432c8a0a24634998e37
Hebash99 <95986694+Hebash99@users.noreply.github.com>
2022-07-17 02:12:22 +0300

    Update stateCityOnly.dart

--- lib/src/stateCityOnly.dart
+++ lib/src/stateCityOnly.dart
@@ -40,6 +40,7 @@ class _CountryStateCityPicker3State extends State<CountryStateCityPicker3> {
   @override
   void initState() {
     super.initState();
+    print(widget.countryID);
    // _getCountry();
     _getState(widget.countryID);
   }
@@ -54,9 +55,10 @@ class _CountryStateCityPicker3State extends State<CountryStateCityPicker3> {
     });
   }
 
- 
+
 
   Future<void> _getState(String countryId)async{
+    print(widget.countryID);
     _stateList.clear();
     _cityList.clear();
     List<StateModel> _subStateList=[];
